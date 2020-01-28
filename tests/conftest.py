dc31c9fbf6ed143517834ed1af81f4794a7ea2d9
ashic <ashic@live.com>
2020-01-28 14:03:39 +0000

    adding a parameterised fixture example

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,18 @@
 import pytest
 from unittest.mock import patch
 
+
 @pytest.fixture(scope='module', autouse=True)
 def foo_settings():
     # the first with is unnecessary, as the function calls some_path, but here to show
-    # multiple patches
-    with patch("patch_test.config.foo_settings.function_to_get_something", return_value="hola"):
+    # multiple patches. Leaving this in can mess up other patches if they patch
+    # the underlying variable, but not the function.
+    # with patch("patch_test.config.foo_settings.function_to_get_something", return_value="hola"):
         with patch("patch_test.config.foo_settings.some_path", "hola"):
+            yield
+
+
+@pytest.fixture(scope="module")
+def foo_settings_w_p(request):
+        with patch("patch_test.config.foo_settings.some_path", request.param):
             yield
--- tests/test_something_w_fixture.py
+++ tests/test_something_w_fixture.py
@@ -1,4 +1,7 @@
 from patch_test import something
+import patch_test
+import pytest
+from unittest.mock import patch
 
 
 def test_something_w_fixture(foo_settings):
@@ -9,3 +12,21 @@ def test_something_w_fixture(foo_settings):
 
     real_2 = something.something()
     assert "The setting is hola" == real_2
+
+
+
+@pytest.mark.parametrize('foo_settings_w_p', ["adios"], indirect=True)
+def test_something_w_fixture_w_p(foo_settings_w_p):
+    real = something.something_w_fn()
+    assert "The setting is adios" == real
+
+    real_2 = something.something()
+    assert "The setting is adios" == real_2
+
+
+    real_3 = patch_test.config.foo_settings.some_path
+    assert "adios" == real_3
+
+    real_4 = patch_test.config.foo_settings.function_to_get_something()
+    assert "adios" == real_4
+
